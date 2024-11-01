3d5ae642e3dda2e5ef76128c5eb2da3a6269e025
Tej <leaf9016@gmail.com>
2024-11-01 17:25:05 +0900

    1. State 관리하기 (완료) 2. 탈출구   1) Ref로 값 참조하기 (완료)

--- src/state/PreservingAndResttingState-1.js
+++ src/state/PreservingAndResttingState-1.js
@@ -27,3 +27,116 @@ function Form() {
   const [text, setText] = useState("");
   return <textarea value={text} onChange={(e) => setText(e.target.value)} />;
 }
+
+export function State2Mine() {
+  const [reverse, setReverse] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  let checkbox = (
+    <label>
+      <input
+        type="checkbox"
+        checked={reverse}
+        onChange={(e) => setReverse(e.target.checked)}
+      />
+      Reverse order
+    </label>
+  );
+  if (reverse) {
+    return (
+      <>
+        <FieldMine
+          label="Last name"
+          text={lastName}
+          onChange={handleLastNameChange}
+        />
+        <FieldMine
+          label="First name"
+          text={firstName}
+          onChange={handleFirstNameChange}
+        />
+        {checkbox}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <FieldMine
+          label="First name"
+          text={firstName}
+          onChange={handleFirstNameChange}
+        />
+        <FieldMine
+          label="Last name"
+          text={lastName}
+          onChange={handleLastNameChange}
+        />
+        {checkbox}
+      </>
+    );
+  }
+}
+
+function FieldMine({ label, text, onChange }) {
+  return (
+    <label>
+      {label}:{" "}
+      <input type="text" value={text} placeholder={label} onChange={onChange} />
+    </label>
+  );
+}
+
+export function State2Answer() {
+  const [reverse, setReverse] = useState(false);
+  let checkbox = (
+    <label>
+      <input
+        type="checkbox"
+        checked={reverse}
+        onChange={(e) => setReverse(e.target.checked)}
+      />
+      Reverse order
+    </label>
+  );
+  if (reverse) {
+    return (
+      <>
+        <FieldAnswer key="lastName" label="Last name" />
+        <FieldAnswer key="firstName" label="First name" />
+        {checkbox}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <FieldAnswer key="firstName" label="First name" />
+        <FieldAnswer key="lastName" label="Last name" />
+        {checkbox}
+      </>
+    );
+  }
+}
+
+function FieldAnswer({ label }) {
+  const [text, setText] = useState("");
+  return (
+    <label>
+      {label}:{" "}
+      <input
+        type="text"
+        value={text}
+        placeholder={label}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </label>
+  );
+}
